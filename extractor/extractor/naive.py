c0e1760812d04ac749b9f6f806115bfd29dccd20
mollnn <1084918073@qq.com>
2021-07-19 08:24:42 +0800

    refactor (extr): 评分算法分离化

--- extractor/extractor/naive.py
+++ extractor/extractor/naive.py
@@ -19,18 +19,32 @@ sys.path.append("..")
 # from matplotlib import pyplot as plt
 
 
+def mark(bvid, duration, frame_total, danmu_list, shotcut_list):
+    # Output: ans: The mark of each frame. len(ans)==frame_total
+    media_file_name = "../../data/media/%s.mp4" % bvid
+    density_narrow = algorithm.danmu.density.calcDanmuDensity(
+        danmu_list, duration, Delta=5)
+    density_wide = algorithm.danmu.density.calcDanmuDensity(
+        danmu_list, duration, Delta=15)
+    ratio = [density_narrow[i]/(density_wide[i]+0.1) for i in range(duration)]
+    sentiments = algorithm.danmu.sentiments.calcDanmuSentiments(
+        danmu_list, duration)
+    ans_per_sec = [ratio[i] * math.sqrt(density_narrow[i]) *
+            (0.5+0.5*math.sqrt(sentiments[i])) for i in range(duration)]
+    ans = []
+    for i in ans_per_sec:
+        ans += [i]*24
+    ans = ans[0:frame_total]
+    ans = scipy.signal.savgol_filter(ans, 49, 3)
+    return ans
+
+
 def solve(bvid):
     print("extractor.naive: Hello ", bvid)
     duration = int(math.ceil(
         float(ffmpeg.probe("../../data/media/%s.mp4" % bvid)["format"]["duration"])))
-    frame_rate = 24
+    frame_rate = 24     # Forced
     frame_total = int(math.ceil(duration*frame_rate))
-
-    # file_danmu = open("../../data/danmu/%s.danmu.json"%bvid,"r",encoding="utf-8")
-    # danmu_list = json.load(file_danmu)
-    # file_danmu.close()
-
-    # shotcut_list = algorithm.shotcut.shotcut.shotcut("../../data/media/%s.mp4"%bvid)
 
     print("extractor.naive: Read danmu and shotcut from database... ", bvid)
     cid = database.msql.query(
@@ -42,59 +56,43 @@ def solve(bvid):
 
     print("extractor.naive: Analysing...")
 
-    # Mark begin
-
-    density_s = algorithm.danmu.density.calcDanmuDensity(
-        danmu_list, duration, Delta=5)
-    density_l = algorithm.danmu.density.calcDanmuDensity(
-        danmu_list, duration, Delta=15)
-    density_p = [density_s[i]/(density_l[i]+0.1) for i in range(duration)]
-    sentiments = algorithm.danmu.sentiments.calcDanmuSentiments(
-        danmu_list, duration)
-    tans = [density_p[i] * math.sqrt(density_s[i]) *
-            (0.5+0.5*math.sqrt(sentiments[i])) for i in range(duration)]
-    ans = []
-    for i in tans:
-        ans += [i]*24
-    ans = ans[0:frame_total]
-    ans = scipy.signal.savgol_filter(ans, 49, 3)
-
-    # Mark end
-
-    # plt.plot(ans)
+    mark_original = mark(bvid, duration, frame_total, danmu_list, shotcut_list)
+    mark_final = mark_original
 
     for shotcut in shotcut_list:
         if shotcut["transition"] == "cut":
             fid = shotcut["cut_frame"]
             if fid < frame_total:
-                ans[fid] = 0
+                mark_final[fid] = 0
             if fid > 0:
-                ans[fid-1] = 0
+                mark_final[fid-1] = 0
         else:
-            fid_l = shotcut["start_frame"]
-            fid_r = shotcut["end_frame"]
-            for i in range(fid_l-1, fid_r+1):
+            frame_id_l = shotcut["start_frame"]
+            frame_id_r = shotcut["end_frame"]
+            for i in range(frame_id_l-1, frame_id_r+1):
                 if i >= 0 and i < frame_total:
-                    ans[i] = 0
+                    mark_final[i] = 0
 
-    thres = algorithm.common.sig.getPropotionPoint(ans, 0.15)
-    bans = [(ans[i] > thres) for i in range(frame_total)]
+    threshold = algorithm.common.sig.getPropotionPoint(mark_final, 0.15)
+    is_frame_good = [(mark_final[i] > threshold) for i in range(frame_total)]
 
-    # plt.plot(bans)
+    # # Visualization
+    # plt.plot(mark_original)
+    # plt.plot(is_frame_good)
     # plt.show()
 
-    res = algorithm.common.sig.makeRanges(bans, 24, 360)
+    result = algorithm.common.sig.makeRanges(is_frame_good, 72, 360)
 
     print("extractor.naive: writing...")
-    for i in res:
+    for i in result:
         xvid = control.xvidgen.generateId()
-        xvobj = {"id": xvid, "bvid": bvid,
+        extraction_obj = {"id": xvid, "bvid": bvid,
                  "frame_begin": i[0], "frame_end": i[1], "src_type": 0, "clip_type": 0}
-        media.editor.edit([{"filename": "../../data/media/%s.mp4" % bvid, "start": xvobj["frame_begin"]/frame_rate,
-                          "duration":(xvobj["frame_end"]-xvobj["frame_begin"])/frame_rate}], "../../data/output/%s.mp4" % xvid, quiet=True)
+        media.editor.edit([{"filename": "../../data/media/%s.mp4" % bvid, "start": extraction_obj["frame_begin"]/frame_rate,
+                          "duration":(extraction_obj["frame_end"]-extraction_obj["frame_begin"])/frame_rate}], "../../data/output/%s.mp4" % xvid, quiet=True)
         os.system("ffmpeg -i ../../data/output/%s.mp4 -r 24 -ss 00:00:00 -vframes 1 ../../data/poster/%s.jpg  -hide_banner -loglevel error" % (xvid, xvid))
         database.msql.query("biliextract", "INSERT INTO extraction (id, bvid, frame_begin, frame_end, src_type, clip_type) VALUES ('%s','%s',%d,%d,%d,%d);" % (
-            xvobj["id"], xvobj["bvid"], xvobj["frame_begin"], xvobj["frame_end"], xvobj["src_type"], xvobj["clip_type"]))
+            extraction_obj["id"], extraction_obj["bvid"], extraction_obj["frame_begin"], extraction_obj["frame_end"], extraction_obj["src_type"], extraction_obj["clip_type"]))
 
     print("extractor.naive: OK!")
 
--- extractor/test/fuck.py
+++ extractor/test/fuck.py
@@ -3,8 +3,8 @@ sys.path.append("..")
 
 import extractor.pipeline
 
-bvid="BV1S4411n71U"
-# extractor.pipeline.downloadMedia(bvid)
-# extractor.pipeline.downloadInfo(bvid)
-# extractor.pipeline.shotCut(bvid)
+bvid="BV1rs411s7Ur"
+extractor.pipeline.downloadMedia(bvid)
+extractor.pipeline.downloadInfo(bvid)
+extractor.pipeline.shotCut(bvid)
 extractor.pipeline.extract(bvid,0,0)
